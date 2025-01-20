9805962a8c103714533fa37bb8441ed371031ab7
vicmtn <victorien.montavon@gmail.com>
2025-01-20 19:08:09 +0100

    feat: Create model for database

---
+++ sql/create.sql
@@ -0,0 +1,26 @@
+-- stations: insert stations
+CREATE TABLE stations (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    long FLOAT NOT NULL,
+    lat FLOAT NOT NULL
+);
+
+-- departures: insert departures from stations
+CREATE TABLE departures (
+    id SERIAL PRIMARY KEY,
+    station_id INT NOT NULL REFERENCES stations(id),
+    departure_station_name TEXT NOT NULL,
+    destination_station_name TEXT NOT NULL,
+    departure_time TIMESTAMP NOT NULL,
+    platform TEXT,
+    sector TEXT
+);
+
+-- trains: insert trains from departures
+CREATE TABLE trains (
+    id SERIAL PRIMARY KEY,
+    departure_id INT NOT NULL REFERENCES departures(id),
+    train_type TEXT,    -- match to 'g" in cff api
+    train_number TEXT   -- match to  "l" in cff api
+);
