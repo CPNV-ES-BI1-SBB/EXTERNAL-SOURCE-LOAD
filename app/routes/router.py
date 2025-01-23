152ece25e6c865d339948c0ce68921f0c14fd149
vicmtn <victorien.montavon@gmail.com>
2025-01-23 18:35:21 +0100

    refactor: Route file

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.routes import process_load
+from app.routes import jobs
 
 api_router = APIRouter()
 
-api_router.include_router(process_load.router, prefix="/load", tags=["ELT Load Operations"])
+api_router.include_router(jobs.router, prefix="/job", tags=["ELT Load Operations"])
